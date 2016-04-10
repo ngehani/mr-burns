72c1b80731af6a91c3cea1143d2a2f5be0a6ad78
kornfeld <lital.kornfeld@hpe.com>
2016-04-10 14:36:50 +0300

    create folder for test results and bind it to the started container

--- testrunner/testrunner.go
+++ testrunner/testrunner.go
@@ -5,24 +5,34 @@ import (
 	"github.com/fsouza/go-dockerclient"
 	"fmt"
 	"log"
+	"os"
 )
 
-func RunTestContainers(client dockerclient.BurnsDockerClient) error {
+func RunTestContainers(client dockerclient.BurnsDockerClient) {
+
 	//Get images with test label and create containers for them
 	imgs, _ := client.ListImages(docker.ListImagesOptions{All: false, Filters:map[string][]string{"label": {"test="}}})
 	log.Printf("images: %+v", imgs)
 	for i, img := range imgs {
-		resultsPath := img.Labels[dockerclient.TestResultsLabel]
-		c := dockerclient.NewContainer(&docker.Container{
-			Name:        fmt.Sprintf("simpsons-%d", i),
-			Config:     &docker.Config{ Image: img.ID },
-			HostConfig: &docker.HostConfig{ Binds: []string{fmt.Sprintf("%s:%s", resultsPath, "/tmp")}},
-		}, &docker.Image{ID:img.ID, }, )
-		err := client.StartContainer(*c)
-		if err != nil {
-			log.Fatal("Failed starting container. ", err)
-			return err
-		}
+		runTestContainer(client, img, fmt.Sprintf("simpsons-%d", i))
+	}
+}
+
+
+func runTestContainer(client dockerclient.BurnsDockerClient, image docker.APIImages, containerName string) error {
+
+	resultsPath := image.Labels[dockerclient.TestResultsLabel]
+	resultDirName := fmt.Sprintf("/tmp/test-results/%s", containerName)
+	os.MkdirAll(resultDirName, 0700)
+	c := dockerclient.NewContainer(&docker.Container{
+		Name:        containerName,
+		Config:     &docker.Config{ Image: image.ID },
+		HostConfig: &docker.HostConfig{ Binds: []string{fmt.Sprintf("%s:%s", resultDirName, resultsPath)}},
+	}, &docker.Image{ID:image.ID, }, )
+	err := client.StartContainer(*c)
+	if err != nil {
+		log.Fatal(fmt.Sprintf("Failed starting container name: %s.", containerName), err)
+		return err
 	}
 
 	return nil
