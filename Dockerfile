488592b21a9cc4ca4ef9f98f5eeae97f15393a7e
Effi <effib@hp.com>
2016-04-17 14:16:51 +0300

    fix build

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,6 @@
 FROM alpine:3.3
 
 WORKDIR /go/src/github.com/gaia-adm/mr-burns
-
 COPY .dist/mr-burns /usr/bin/mr-burns
 
-CMD ["--help"]
 ENTRYPOINT ["/usr/bin/mr-burns"]
--- script/go_build.sh
+++ script/go_build.sh
@@ -6,7 +6,7 @@ go_build() {
   rm -rf "${distdir}"
   mkdir "${distdir}"
   go get
-  go build -v -o ../${distdir}/mr-burns
+  go build -v -o ${distdir}/mr-burns
 }
 
 go_build
