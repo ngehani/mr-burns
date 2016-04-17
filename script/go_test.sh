3c67328cb71368b867657af43ef88fa14ac16bdc
kornfeld <lital.kornfeld@hpe.com>
2016-04-17 16:46:11 +0300

    update go_test

--- script/go_test.sh
+++ script/go_test.sh
@@ -6,7 +6,8 @@ generate_test_coverage_data() {
     
 	rm -rf "$workdir"
     mkdir "$workdir"
-    go test -cover -v | go-junit-report > "$workdir/go-results_tests.xml"
+    cd dockerclient
+    go test -cover -v | go-junit-report > "../$workdir/go-results_tests.xml"
 }
 
 generate_test_coverage_data
