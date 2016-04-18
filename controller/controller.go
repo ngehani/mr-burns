36f7970f9131251255f595bc4af5f36673089b77
kornfeld <lital.kornfeld@hpe.com>
2016-04-18 10:23:24 +0300

    check if containerCMD is not empty before applying, extract code to methods

--- controller/controller.go
+++ controller/controller.go
@@ -9,6 +9,7 @@ import (
 	"io/ioutil"
 	"bytes"
 	"net/http"
+	"image"
 )
 
 func Start(client dockerclient.DockerClient) {
@@ -28,35 +29,58 @@ func runTestContainer(client dockerclient.DockerClient, image docker.APIImages, 
 	containerCmd := image.Labels[dockerclient.TestCmdLabel]
 	resultDirName := fmt.Sprintf("/tmp/test-results/%s", containerName)
 	os.MkdirAll(resultDirName, 0700)
+	containerConfig := &docker.Config{Image: image.ID }
+	if len(containerCmd) > 0 {
+		containerConfig.Cmd = []string{containerCmd }
+	}
+	startContainer(client,
+		containerName,
+		containerConfig,
+		&docker.HostConfig{Binds: []string{fmt.Sprintf("%s:%s", resultDirName, containerResultsPath)}},
+		image)
+
+	status, err := client.WaitContainer(containerName)
+	if err != nil {
+		log.Fatal(fmt.Sprintf("Failed waiting for container: %s.", containerName), err)
+		return err
+	}
+	if (status == 0) {
+		postResults(resultDirName, containerResultsFile)
+	}
+
+	return nil
+}
+
+func startContainer(client dockerclient.DockerClient, containerName string, containerConfig *docker.Config, hostConfig *docker.HostConfig, image docker.APIImages) error {
+
 	client.RemoveContainer(containerName, true)
 	c := dockerclient.NewContainer(&docker.Container{
 		Name:        containerName,
-		Config:     &docker.Config{Image: image.ID, Cmd:[]string{containerCmd } },
-		HostConfig: &docker.HostConfig{Binds: []string{fmt.Sprintf("%s:%s", resultDirName, containerResultsPath)}},
+		Config:     containerConfig,
+		HostConfig: hostConfig,
 	}, &docker.Image{ID:image.ID, }, )
 	err := client.StartContainer(*c)
 	if err != nil {
 		log.Fatal(fmt.Sprintf("Failed starting container: %s.", containerName), err)
 		return err
 	}
-	status, err := client.WaitContainer(containerName)
-	if (status == 0) {
-		containerTestResults, err := ioutil.ReadFile(fmt.Sprintf("%s/%s", resultDirName, containerResultsFile))
-		if (err != nil) {
-			log.Fatal("Failed to read file", err)
-		}
-		req, err := http.NewRequest("POST", "http://distributor-link:8000", bytes.NewBuffer(containerTestResults))
-		client := &http.Client{}
-		resp, err := client.Do(req)
-		if err != nil {
-			log.Fatal("Failed to POST container test results", err)
-		}
-		defer resp.Body.Close()
-	}
-	if err != nil {
-		log.Fatal(fmt.Sprintf("Failed waiting for container: %s.", containerName), err)
+}
+
+func postResults(resultDirName string, containerResultsFile string) error {
+
+	containerTestResults, err := ioutil.ReadFile(fmt.Sprintf("%s/%s", resultDirName, containerResultsFile))
+	if (err != nil) {
+		log.Fatal("Failed to read file", err)
 		return err
 	}
+	req, err := http.NewRequest("POST", "http://distributor-link:8000", bytes.NewBuffer(containerTestResults))
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Fatal("Failed to POST container test results", err)
+		return err
+	}
+	defer resp.Body.Close()
 
 	return nil
 }
