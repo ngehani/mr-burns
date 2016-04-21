dcce7e105fcffd624c5ea8ab359d78e000fc3979
Effi <effib@hp.com>
2016-04-21 13:47:23 +0300

    back to glide

--- script/go_test.sh
+++ script/go_test.sh
@@ -7,9 +7,8 @@ generate_test_coverage_data() {
     # create dir if not exist and remove old content
     mkdir "$workdir"
 	rm -rf "$workdir/*"
-    #glide install
+    glide install
     cd controller
-    go get
     go test -cover -v > "../$workdir/go-results_tests.txt"
     cat "../$workdir/go-results_tests.txt" | go-junit-report > "../$workdir/go-results_tests.xml"
 }
